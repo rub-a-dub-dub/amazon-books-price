cb0c3fe127a4290cf9ecdd396764f51ba534444b
rub-a-dub-dub <rub-a-dub-dub@users.noreply.github.com>
2014-08-16 23:53:36 -0700

    Accidentally left the depth limit on.

--- amazonCategories/step1.sh
+++ amazonCategories/step1.sh
@@ -2,5 +2,5 @@
 
 # This is the first step in the pipeline
 rm cats.*
-scrapy crawl categories -o cats.json -s DOWNLOAD_DELAY=0.05 -s DEPTH_LIMIT=1 -s LOG_FILE=cats.log -s USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
+scrapy crawl categories -o cats.json -s DOWNLOAD_DELAY=0.1 -s DEPTH_LIMIT=0 -s LOG_FILE=cats.log -s USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
 mv cats.* /vagrant
