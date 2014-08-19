a8d229f2f41f1fb9b69f39755170432e56b85357
rub-a-dub-dub <rub-a-dub-dub@users.noreply.github.com>
2014-08-18 17:09:01 -0700

    Increased download delay.

--- amazonCategories/step3.sh
+++ amazonCategories/step3.sh
@@ -2,5 +2,5 @@
 
 # This is the third step in the pipeline
 rm books.*
-scrapy crawl bookListCategory -o books.json -s DOWNLOAD_DELAY=0.1 -s DEPTH_LIMIT=0 -s LOG_FILE=books.log -s USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
+scrapy crawl bookListCategory -o books.json -s DOWNLOAD_DELAY=5 -s DEPTH_LIMIT=0 -s LOG_FILE=books.log -s USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
 mv books.* /vagrant
